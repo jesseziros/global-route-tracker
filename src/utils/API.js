9e62fccb0d9473fafe9f6a98a40c4224a6f5c448
jesseziros <jtziros2020@gmail.com>
2022-03-18 12:08:22 -0400

    Cleaning up project and starting Api call to access mapbox

---
+++ src/utils/API.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactMapGl from 'react-map-gl';
+
+
+const mapboxToken = "pk.eyJ1IjoiamVzc2V6aXJvcyIsImEiOiJjbDB2ZGM4MDMxNjRmM2NwazhiYnA3OW1iIn0.OstibnuXEjU_QZRCDuGSsw";
+
+class Map extends Component {
+    constructor() {
+        
+    }
+}
